a2a4e16663437665f3bc12f047e6fb53ec57d831
Krishna Rudani <krishna.rudani@somaiya.com>
2022-11-12 19:04:29 +0530

    check currency

--- Mini Project/src/js/script.js
+++ Mini Project/src/js/script.js
@@ -1,46 +1,33 @@
-const currencyEl_one = document.getElementById('currency-one');
-const amountEl_one = document.getElementById('amount-one');
-const currencyEl_two = document.getElementById('currency-two');
-const amountEl_two = document.getElementById('amount-two');
-const rateEl = document.getElementById('rate');
-const swap = document.getElementById('swap');
+const input_currency = document.querySelector('#input_currency');
+const output_currency = document.querySelector('#output_currency');
+const input_amount = document.querySelector('#input_amount');
+const output_amount = document.querySelector('#output_amount');
+const exchange = document.querySelector('#exchange');
+const rate = document.querySelector('#rate');
 
-async function calculate() {
-    const currency_one = currencyEl_one.value;
-    const currency_two = currencyEl_two.value;
-    const res = await fetch('https://open.exchangerate-api.com/v6/latest');
-    // console.log('res', res);
-    const data = await res.json();
-    // console.log('data', data);
-    const rate = data.rates[currency_two] / data.rates[currency_one];
-    rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
-    amountEl_two.value = (amountEl_one.value * (rate)).toFixed(2);
+input_currency.addEventListener('change', compute);
+output_currency.addEventListener('change', compute);
+input_amount.addEventListener('input', compute);
+output_amount.addEventListener('input', compute);
+
+exchange.addEventListener('click', ()=>{
+    const temp = input_currency.value;
+    input_currency.value = output_currency.value;
+    output_currency.value= temp;
+    compute();
+});
+
+function compute(){
+    const input_currency1 = input_currency.value;
+    const output_currency1 = output_currency.value;
+
+    fetch(`https://api.exchangerate-api.com/v4/latest/${input_currency1}`)
+    .then(res => res.json())
+    .then(res => {
+        const new_rate = res.rates[output_currency1];
+        rate.innerText = `1 ${input_currency1} = ${new_rate} ${output_currency1}`
+        output_amount.value = (input_amount.value * new_rate).toFixed(2);
+    })
 }
 
-currencyEl_one.addEventListener('change', calculate);
-amountEl_one.addEventListener('input', calculate);
-currencyEl_two.addEventListener('change', calculate);
-amountEl_two.addEventListener('input', calculate);
-
-calculate();
-
-// swap.addEventListener('click', async () => {
-//     const currency_one = currencyEl_two.value;
-//     const currency_two = currencyEl_one.value;
-//     const res = await fetch('https://open.exchangerate-api.com/v6/latest');
-//     // console.log('res', res);
-//     const data = await res.json();
-//     // console.log('data', data);
-//     const rate = data.rates[currency_two] / data.rates[currency_one];
-//     rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
-//     amountEl_two.value = (amountEl_one.value * (rate)).toFixed(2);
-//     currencyEl_one.value = currency_one;
-//     currencyEl_two.value = currency_two
-// })
-
-swap.addEventListener('click', () => {
-  const temp = currencyEl_one.value;
-  currencyEl_one.value = currencyEl_two.value;
-  currencyEl_two.value = temp;
-  calculate();
-});
+compute();
